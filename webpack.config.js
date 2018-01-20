c8d7738e999061e49872e91ed0af1736600a405b
Your Name <you@example.com>
2018-01-20 16:09:04 +0100

    Add files to project

--- webpack.config.js
+++ webpack.config.js
@@ -2,28 +2,30 @@ const path = require('path');
 
 //webpack.config.js
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'app.bundle.js'
-    }
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: 'app.bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /(node_modules|bower_components)/
+			},
+			{
+				test: /\.css$/,
+				use: [
+					{ loader: 'style-loader'},
+					{
+						loader: 'css-loader',
+						options: {
+								modules: true
+						}
+					}
+				]
+			}
+	  	]	  
+	}
 };
-
-rules: [
-    {
-        test: /\.js$/,
-        loader: "babel-loader"
-    },
-    {
-        test: /\.css$/,
-        use: [
-            { loader: 'style-loader'},
-            {
-                loader: 'css-loader',
-                options: {
-                    modules: true
-                }
-            }
-        ]
-    }
-]
